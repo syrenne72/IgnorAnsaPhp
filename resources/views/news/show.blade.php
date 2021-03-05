3cb3ec2b827758bc72f489f52ead5cad23fb2bdc
syrenne <valey16@gmail.cpm>
2021-03-05 12:03:04 +0100

    Restyle of show.blade.php

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -4,30 +4,23 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-6 w-25">
-            <img src="/storage/{{$news->foto}}" alt="" style="width: 100%">
+        <div class="col-4 w-25">
+            <img src="/storage/{{$news->foto}}" alt="" style="width: 75%">
         </div>
         <div class="col-6 w-50">
             <div>
-                <h3>{{$news->titolo}}</h3>
-                <p>{{$news->descrizione}}</p>
-            </div>
-        </div>
+                <div class="font-weight-bold h1">{{$news->titolo}}</div>
+                <div class="font-weight-normal h4">{{$news->descrizione}}</div>
+                <p>
+                    {{$news->articolo}}
+                </p>
 
-        <div class="row mt-4 align-items-center">
-            <span class="col-2">
                 <img src="{{$news->user->profileImage()}}" class="rounded-circle" style="max-height: 40px;">
-            </span>
-            <span class="col-4 font-weight-bold">
                 <a href="/profile/{{$news->user->id}}">
-                    <span class="text-dark">{{$news->user->username}}</span>
+                    <span class="text-dark font-weight-bold">{{$news->user->nome}}</span>
                 </a>
-            </span>
-            <div class="col-8">
-                <div>{{$news->user->bio}}</div>
             </div>
         </div>
-
     </div>
 </div>
 @endsection
