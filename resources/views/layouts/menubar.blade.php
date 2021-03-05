60982da1c30b61bf323afa2c1ebbea64327ecc3c
syrenne <valey16@gmail.cpm>
2021-03-05 09:42:13 +0100

    Adapted footer menu bar

--- resources/views/layouts/menubar.blade.php
+++ resources/views/layouts/menubar.blade.php
@@ -97,6 +97,97 @@
         @yield('content')
     </main>
 
+<!-- Footer -->
+    <div class="container-fluid fh5co_footer_bg pb-3">
+        <div class="container animate-box">
+            <div class="row">
+                <div class="col-12 spdp_right py-5"></div>
+                <div class="clearfix"></div>
+
+                <!-- Prima colonna footer -->
+                <div class="col-12 col-md-4 col-lg-4">
+                    <div class="footer_main_title py-3"> Su di noi</div>
+                    <div class="footer_sub_about pb-3">
+                        IgnorAnsa è più di un semplice sito di notizie: è un esperimento sociale. Il suo scopo
+                        è sdoganare il mito secondo cui tutto ciò che esiste sul web è vero.
+                    </div>
+                    <div class="footer_mediya_icon">
+                        <div class="text-center d-inline-block"><a class="fh5co_display_table_footer">
+                                <div class="fh5co_verticle_middle"><i class="fa fa-linkedin"></i></div>
+                            </a></div>
+                        <div class="text-center d-inline-block"><a class="fh5co_display_table_footer">
+                                <div class="fh5co_verticle_middle"><i class="fa fa-twitter"></i></div>
+                            </a></div>
+                        <div class="text-center d-inline-block"><a class="fh5co_display_table_footer">
+                                <div class="fh5co_verticle_middle"><i class="fa fa-facebook"></i></div>
+                            </a></div>
+                    </div>
+                </div>
+                <!-- -->
+
+                <!-- Seconda colonna footer -->
+                <div class="col-12 col-md-3 col-lg-3">
+                    <div class="footer_main_title py-3"> Categorie</div>
+                    <ul class="footer_menu">
+                        <li><a href="#" class=""><i class="fa fa-angle-right"></i>&nbsp;&nbsp; Business</a></li>
+                        <li><a href="#" class=""><i class="fa fa-angle-right"></i>&nbsp;&nbsp; Entertainment</a></li>
+                        <li><a href="#" class=""><i class="fa fa-angle-right"></i>&nbsp;&nbsp; Environment</a></li>
+                        <li><a href="#" class=""><i class="fa fa-angle-right"></i>&nbsp;&nbsp; Health</a></li>
+                        <li><a href="#" class=""><i class="fa fa-angle-right"></i>&nbsp;&nbsp; Life style</a></li>
+                        <li><a href="#" class=""><i class="fa fa-angle-right"></i>&nbsp;&nbsp; Politics</a></li>
+                        <li><a href="#" class=""><i class="fa fa-angle-right"></i>&nbsp;&nbsp; Technology</a></li>
+                        <li><a href="#" class=""><i class="fa fa-angle-right"></i>&nbsp;&nbsp; World</a></li>
+                    </ul>
+                </div>
+                <!-- -->
+
+                <!-- Terza colonna foooter -->
+                <div class="col-12 col-md-5 col-lg-4 position_footer_relative">
+                    <div class="footer_main_title py-3"> Post popolari</div>
+                    <div class="footer_makes_sub_font"> Dec 31, 2016</div>
+                    <a href="#" class="footer_post pb-4"> Success is not a good teacher failure makes you humble </a>
+                    <div class="footer_makes_sub_font"> Dec 31, 2016</div>
+                    <a href="#" class="footer_post pb-4"> Success is not a good teacher failure makes you humble </a>
+                    <div class="footer_makes_sub_font"> Dec 31, 2016</div>
+                    <a href="#" class="footer_post pb-4"> Success is not a good teacher failure makes you humble </a>
+                </div>
+                <!-- -->
+
+            </div>
+
+            <!-- Campo di registrazione -->
+            <div class="row justify-content-center pt-2 pb-4">
+                <div class="col-12 col-md-8 col-lg-7 ">
+                    <div class="input-group">
+                        <span class="input-group-addon fh5co_footer_text_box" id="basic-addon1"><i class="fa fa-envelope"></i></span>
+                        <input type="text" class="form-control fh5co_footer_text_box" placeholder="email..." aria-describedby="basic-addon1">
+                        <a href="#" class="input-group-addon fh5co_footer_subcribe" id="basic-addon12"> <i class="fa fa-paper-plane-o"></i>&nbsp;&nbsp;Subscribe</a>
+                    </div>
+                </div>
+            </div>
+            <!-- -->
+
+        </div>
+    </div>
+    <div class="container-fluid fh5co_footer_right_reserved">
+        <div class="container">
+            <div class="row  ">
+                <div class="col-12 col-md-6 py-4 Reserved"> © Copyright 2021, All rights reserved.</div>
+                <div class="col-12 col-md-6 spdp_right py-4">
+                    <a href="/journal/home" class="footer_last_part_menu">Home</a>
+                    <a href="Contact_us.html" class="footer_last_part_menu">About</a>
+                    <a href="Contact_us.html" class="footer_last_part_menu">Contact</a>
+                    <a href="blog.html" class="footer_last_part_menu">Latest News</a></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="gototop js-top">
+        <a href="#" class="js-gotop"><i class="fa fa-arrow-up"></i></a>
+    </div>
+    <!-- -->
+
+    <!-- Script -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="/js/owl.carousel.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
@@ -112,5 +203,6 @@
     <!-- Main -->
     <script src="/js/main.js"></script>
     <script>if (!navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i)){$(window).stellar();}</script>
+<!-- -->
 </body>
 </html>
